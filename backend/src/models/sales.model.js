b457ae614fc0a237f1da1e93475b75fd479632fb
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-09-14 21:37:57 -0300

    remove lib camelize and add function toCamelCase

--- backend/src/models/sales.model.js
+++ backend/src/models/sales.model.js
@@ -1,19 +1,31 @@
-const camelize = require('camelize');
 const connection = require('./connection');
+
+const mapKeysToCamelCase = (obj) => {
+    if (!obj) return obj;
+
+    const camelCaseObj = {};
+    Object.keys(obj).forEach((key) => {
+        const camelCaseKey = key.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+        camelCaseObj[camelCaseKey] = obj[key];
+    });
+    return camelCaseObj;
+};
 
 const getAll = async () => {
     const [sales] = await connection.execute(
-        `SELECT sp.sale_id, sp.product_id, sp.quantity, s.date FROM
+        `SELECT sp.sale_id as saleId, sp.product_id as productId, sp.quantity, s.date FROM
         sales_products sp
         INNER JOIN sales s 
-        ON sp.sale_id = s.id `,
+        ON sp.sale_id = s.id`,
     );
-    return camelize(sales);
+
+    const camelCaseSales = sales.map(mapKeysToCamelCase);
+    return camelCaseSales;
 };
 
 const findById = async (id) => {
     const [sale] = await connection.execute(
-        `SELECT s.date, sp.product_id, sp.quantity
+        `SELECT s.date, sp.product_id as productId, sp.quantity
         FROM sales s
         INNER JOIN sales_products sp ON s.id = sp.sale_id
         WHERE s.id = ?
@@ -21,7 +33,9 @@ const findById = async (id) => {
         `,
         [id],
     );
-    return camelize(sale);
+
+    const camelCaseSale = sale.map(mapKeysToCamelCase);
+    return camelCaseSale;
 };
 
 module.exports = {
